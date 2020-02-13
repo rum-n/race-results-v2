cac4ffe6bd3c973d8bdde675192c9dd8d1d594f7
rum-n <manev.ru@gmail.com>
2020-02-13 13:35:36 +0200

    transforming time

--- sofiaMarathon2016/sofiaMarathon2016.js
+++ sofiaMarathon2016/sofiaMarathon2016.js
@@ -1,47 +1,60 @@
 
 const MALE_DATA = 'sofiaMarathon_male_2016.csv';
 
-        // set the dimensions and margins of the graph
-        const margin = {top: 20, right: 30, bottom: 30, left: 60},
-            width = 860 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
-        
-        // append the svg object to the body of the page
-        const svg = d3.select("#sofiaMarathon_2016_viz")
-        .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
-        
-        //Read the male data 
-        d3.csv(MALE_DATA, function(data) {
-        
-        // Add X axis
-        const x = d3.scaleTime()
-            .range([ 0, width ]);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-        
-        // Add Y axis
-        const y = d3.scaleLinear()
-            .range([ height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
-        
-        // Add dots
-        svg.append('g')
-            .selectAll("dot")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("cx", function (d) { return x(d.Age); } )
-            .attr("cy", function (d) { return y(d.Time); } )
-            .attr("r", 1.5)
-            .style("fill", "#69b3a2")
-        });
+            // set the dimensions and margins of the graph
+                const margin = {top: 20, right: 30, bottom: 30, left: 60},
+                    width = 860 - margin.left - margin.right,
+                    height = 500 - margin.top - margin.bottom;
+                
+                // append the svg object to the body of the page
+                const svg = d3.select("#sofiaMarathon_2016_viz")
+                .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                    .attr("transform",
+                        "translate(" + margin.left + "," + margin.top + ")");
+                
+                //Read the male data 
+                d3.csv(MALE_DATA, function(data) {
+                
+                // Add X axis
+                const x = d3.scaleLinear()
+                    .range([ 0, width ]);
+                svg.append("g")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(d3.axisBottom(x));
+                
+                // Add Y axis
+                const y = d3.scaleLinear()
+                    .range([ height, 0]);
+                svg.append("g")
+                    .call(d3.axisLeft(y));
+                
+                // Add dots
+                svg.append('g')
+                    .selectAll("dot")
+                    .data(data)
+                    .enter()
+                    .append("circle")
+                    .attr("cx", function (d) { return x(d.Age); } )
+                    .attr("cy", function (d) { return y(d.Time); } )
+                    .attr("r", 1.5)
+                    .style("fill", "#69b3a2")
+                })
+
+                // Number.prototype.toHHMMSS = function () {
+                //     var sec_num = parseInt(this, 10); // don't forget the second param
+                //     var hours   = Math.floor(sec_num / 3600);
+                //     var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+                //     var seconds = sec_num - (hours * 3600) - (minutes * 60);
+            
+                //     if (hours   < 10) {hours   = hours;}
+                //     if (minutes < 10) {minutes = "0"+minutes;}
+                //     if (seconds < 10) {seconds = "0"+seconds;}
+                //     var time    = hours+':'+minutes+':'+seconds;
+                //     return time;
+                // }
 
 
         // set the dimensions and margins of the graph
